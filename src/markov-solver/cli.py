cd1d783bcc15bff54a944a1d2bb51d8612ab783d
Giacomo Marciani <mgiacomo@amazon.com>
2021-09-12 18:06:25 +0200

    [CLI] First stab at the CLI

---
+++ src/markov-solver/cli.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import click
+from utils import guiutils, logutils
+
+logger = logutils.get_logger(__name__)
+
+
+@click.group(invoke_without_command=True, context_settings=dict(max_content_width=120))
+@click.option("--debug/--no-debug", default=False, show_default=True, type=bool, help="Activate/Deactivate debug mode.")
+@click.pass_context
+@click.version_option(version="0.0.1")
+def main(ctx, debug):
+    print(guiutils.get_splash())
+    if ctx.invoked_subcommand is None:
+        print(ctx.get_help())
+    else:
+        logutils.set_log_level(logger, "DEBUG" if debug else "INFO")
+        logger.debug("Debug Mode: {}".format("on" if debug else "off"))
+
+
+if __name__ == "__main__":
+    main(obj={})
---
+++ src/markov-solver/utils/guiutils.py
@@ -0,0 +1,49 @@
+"""
+Utilities for CLI.
+"""
+
+import sys
+
+from colored import attr, fg
+from logutils import get_logger
+from pyfiglet import Figlet
+
+logger = get_logger(__name__)
+
+
+def get_splash():
+    """
+    Returns the Splash Screen as ASCII Art.
+    :return: The Splash Screen.
+    """
+    f = Figlet(font="slant")
+    return "%s %s %s" % (fg("yellow"), f.renderText("Markov Solver"), attr(0))
+
+
+def print_progress(iteration, total, prefix="PROGRESS", suffix="Complete", message="", decimals=0, bar_length=50):
+    format_string = "{0:." + str(decimals) + "f}"
+    percents = format_string.format(100 * (iteration / float(total)))
+    filled_length = int(round(bar_length * iteration / float(total)))
+    bar = "=" * filled_length + " " * (bar_length - filled_length)
+
+    sys.stdout.flush()
+
+    sys.stdout.write(
+        "\r%s [%s] %s%% %s (%d/%d) { %s }" % (prefix, bar, percents, suffix, iteration, total, message or "no message")
+    )
+
+
+if __name__ == "__main__":
+    print(get_splash())
+
+    from time import sleep
+
+    tot = 1000
+
+    logger.info("Start")
+
+    for i in range(tot + 1):
+        sleep(1 / tot)
+        print_progress(i, tot)
+
+    logger.info("End")
