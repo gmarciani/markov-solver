3bffe80cbecc5b4b543027d12a0d27c04e8f709b
Giacomo Marciani <mgiacomo@amazon.com>
2024-08-01 01:27:00 +0200

    [Test] Expand the solve unit tests to cover both the simple and the symbolic definitions.

--- tests/markov_solver/test_cli.py
+++ tests/markov_solver/test_cli.py
@@ -28,23 +28,38 @@ def test_solve_command_no_args(runner):
     assert "Error: Missing option '--definition'" in result.output
 
 
-def test_solve_command_with_args(runner, resource_path_root, tmp_path):
-    definition_file = resource_path_root.joinpath(
-        "definitions/simple/simple.definition.yaml"
-    )
-    with open(
-        str(resource_path_root.joinpath("definitions/simple/expected-solution.txt")),
-        "r",
-    ) as file:
-        expected_solution = file.read()
-
+@pytest.mark.parametrize(
+    "definition_file, expected_exit_code, expected_probabilities",
+    [
+        (
+                "definitions/simple/simple.definition.yaml",
+                0,
+                [
+                    r"Rainy.+0\.166666666666667",
+                    r"Sunny.+0\.833333333333333"
+                ]
+        ),
+        (
+                "definitions/symbolic/symbolic.definition.yaml",
+                0,
+                [
+                    r"0.+0\.475836431226766",
+                    r"1.+0\.356877323420074",
+                    r"2.+0\.133828996282528",
+                    r"3.+0\.0334572490706320"
+                ]
+        ),
+    ],
+)
+def test_solve_command_with_args(runner, resource_path_root, tmp_path, definition_file, expected_exit_code, expected_probabilities):
+    definition_file_path = resource_path_root.joinpath(definition_file)
     outdir = tmp_path / "output"
-    result = runner.invoke(
-        main, ["solve", "--definition", str(definition_file), "--outdir", str(outdir)]
-    )
-    assert_that(result.exit_code).is_equal_to(0)
-    assert_that(result.output).matches(r"Rainy.+0\.166666666666667")
-    assert_that(result.output).matches(r"Sunny.+0\.833333333333333")
+
+    result = runner.invoke(main, ["solve", "--definition", str(definition_file_path), "--outdir", str(outdir)])
+
+    assert_that(result.exit_code).is_equal_to(expected_exit_code)
+    for expected_probability in expected_probabilities:
+        assert_that(result.output).matches(expected_probability)
 
 
 if __name__ == "__main__":
