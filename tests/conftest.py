6841ebe17af86a732a4485abef259c6ff0ee42f7
Giacomo Marciani <giacomo.marciani@gmail.com>
2026-02-08 01:05:48 -0500

    [Test] Fix import of tests resources.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ from pathlib import Path
 
 @pytest.fixture
 def resource_path_root() -> Path:
-    """Return the path to the test resources directory."""
-    return Path(__file__).parent / "testresources"
+    """Return the path to the test resources' directory."""
+    return Path(__file__).parent / "resources"
